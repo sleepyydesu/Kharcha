8fbffb36536ebdf143e13e5a535d434e3d2c30b0
Rachit Manandhar <rachit2063@gmail.com>
2026-04-11 17:10:23 +0545

    Add production server to the backend swagger

--- Backend/src/swagger.js
+++ Backend/src/swagger.js
@@ -13,6 +13,10 @@ const swaggerSpec = {
             url: `http://localhost:${process.env.PORT || 5000}`,
             description: "Local development server",
         },
+        {
+            url: `https://kharcha-production.up.railway.app`,
+            description: "Production server",
+        },
     ],
     components: {
         securitySchemes: {
